fd3d52492472f0e884f1c15c22063e6dfd514f40
MohaimenH <mohaimenhassan08@gmail.com>
2021-07-10 14:34:14 +0600

    Finished Menu Page

--- javascript/restaurant-page/src/index.js
+++ javascript/restaurant-page/src/index.js
@@ -7,11 +7,10 @@ import contactPage from './contactPage';
 const root = document.querySelector(".content");
 
 constantDisplay(root);
-homePage(root);
+homePage(root); // Working on Home
+const navList = document.querySelector(".nav-list");
 
 const eventListeners = (() => {
-    const navList = document.querySelector(".nav-list");
-
     navList.addEventListener('click', (e) => {
         if (e.target.tagName.toLowerCase() === 'li') {
             switchDisplay(e.target.textContent.toLowerCase());
@@ -19,20 +18,32 @@ const eventListeners = (() => {
     });
 })();
 
+const toggleUnderline = (toUnderline) => {
+    let menuItems = navList.childNodes;
+    for (let item of menuItems) {
+        item.classList.remove('menu-underline');
+    }
+
+
+    toUnderline.classList.add('menu-underline');
+};
+
 const switchDisplay = (target) => {
 
     root.removeChild(root.lastChild);
-
     if (target === 'home') {
         homePage(root);
+        toggleUnderline(document.querySelector('.menu-home'));
     }
 
     else if (target === 'menu') {
         menuPage(root);
+        toggleUnderline(document.querySelector('.menu-menu'));
     }
 
     else if (target === 'contact') {
         contactPage(root);
+        toggleUnderline(document.querySelector('.menu-contact'));
     }
 
     else {
--- javascript/restaurant-page/src/menuPage.js
+++ javascript/restaurant-page/src/menuPage.js
@@ -1,10 +1,43 @@
+const burgers = [{name: 'The Classic', description: 'Classic Chicken, Special Mayo', price: 130}, {name: 'BBQ Big', description: 'Loaded Chicken, BBQ & Special Sauce', price: 155}, {name: 'Beef Burger', description: 'Beef Patty, Cheese, Special Sauce', price: 130}, {name: 'Rooster Burger', description: 'Loaded Chicken, Cheese, Special Sauce', price: 155}];
+
+const generateCards = () => {
+    const holderDiv = document.createElement('div');
+    holderDiv.classList.add('holder-div-menu');
+
+    for (let burger of burgers) {
+        let itemCard = document.createElement('div');
+        itemCard.classList.add('menu-item-card');
+        let name = document.createElement('div');
+        let desc = document.createElement('div');
+        let price = document.createElement('div');
+
+        name.textContent = burger.name;
+        desc.textContent = burger.description;
+        price.textContent = `${burger.price}TK`;
+
+        itemCard.appendChild(name);
+        itemCard.appendChild(desc);
+        itemCard.appendChild(price);
+
+        holderDiv.appendChild(itemCard);
+    }
+
+    return holderDiv;
+};
+
 const menuPage = (root) => {
     const outerDiv = document.createElement('div');
-    outerDiv.classList.add('outerDiv');
-    const introText = document.createElement('p');
-    introText.textContent = "Menu Page.";
+    outerDiv.classList.add('outerDiv', 'menu-page-div');
+    
+    const burgerSection = document.createElement('section');
 
-    outerDiv.appendChild(introText);
+    const burgersHeader = document.createElement('h2');
+    burgersHeader.textContent = "Burgers";
+    burgersHeader.classList.add('menu-type-heading');
+
+    outerDiv.appendChild(burgersHeader);
+    outerDiv.appendChild(generateCards());
+
     root.appendChild(outerDiv);
 }
 
