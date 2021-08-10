366706b2bd0c35c30cadc0d0a25c2d8e4ae1e632
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-10 20:10:46 +0600

    Updated API Key

--- javascript/weather-app/script.js
+++ javascript/weather-app/script.js
@@ -1,7 +1,15 @@
+let API_KEY;
+
+fetch("apikey.json")
+    .then((res) => res.json())
+    .then((data) => {
+        API_KEY = data.WEATHER_API_KEY;
+    });
+
 const currentWeather = async (place) => {
     const data = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${String(
             place
-        )}&appid=REDACTED`
+        )}&appid=${API_KEY}`
     );
 };
