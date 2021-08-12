877f8538f514512828e9db906c5bd18239a08ad4
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-12 20:18:51 +0600

    Added search

--- javascript/giphy-api/giphy.js
+++ javascript/giphy-api/giphy.js
@@ -1,6 +1,8 @@
 const API_KEY = GIPHY_API_KEY;
 const img = document.querySelector(".image-holder");
 
+let currentGIF = "cats";
+
 const searchGIF = async (search) => {
     const fetchPromise = await fetch(
         `http://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search}`,
@@ -15,9 +17,15 @@ const searchGIF = async (search) => {
     // }); // using `then`
 };
 
-searchGIF("cats");
+searchGIF(currentGIF);
 
 document.querySelector(".refresh-btn").addEventListener("click", (e) => {
     e.preventDefault();
-    searchGIF("cats");
+    searchGIF(currentGIF);
+});
+
+document.querySelector(".search-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    currentGIF = document.querySelector(".search-input").value;
+    searchGIF(currentGIF);
 });
