b8c8800a50b1daa97b7869cca7fbdcadb611d916
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-14 00:25:26 +0600

    Added styling

--- javascript/weather-app/script.js
+++ javascript/weather-app/script.js
@@ -10,21 +10,44 @@ const weatherInfo = document.querySelector(".weather-info");
 
 const currentWeather = async (place) => {
     weatherInfo.textContent = "Loading...";
-    const data = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${String(
-            place
-        )}&appid=${API_KEY}`
-    );
 
-    return await data.json();
+    try {
+        const data = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${String(
+                place
+            )}&appid=${API_KEY}`
+        );
+
+        return await data.json();
+    } catch {
+        return 0;
+    }
 };
 
-let placeInput = document.querySelector(".info-form");
+const placeInput = document.querySelector(".info-form");
+const mainHeader = document.querySelector(".main-header");
 
 placeInput.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    weatherInfo.textContent = JSON.stringify(
-        await currentWeather(document.querySelector(".place").value)
-    );
+    let place = document.querySelector(".place").value;
+    // let weatherData = await currentWeather(
+    //     document.querySelector(".place").value
+    // );
+
+    // if (weatherData == 0) {
+    //     weatherInfo.textContent = "Error";
+    //     return;
+    // }
+
+    // if (Number(weatherData.cod) == 404) {
+    //     weatherInfo.textContent = String(weatherData.message);
+    //     return;
+    // }
+
+    // weatherInfo.textContent = JSON.stringify(weatherData);
+
+    weatherInfo.textContent = "Loaded.";
+
+    mainHeader.textContent = `${place}'s Weather`;
 });
