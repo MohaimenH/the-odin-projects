2cbf3101e07d0a14a3eb0f7a3f4d272390879dff
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-15 12:20:15 +0600

    Finished Mobile Version of Weather App

--- javascript/weather-app/script.js
+++ javascript/weather-app/script.js
@@ -9,47 +9,46 @@ fetch("apikey.json")
 const weatherInfo = document.querySelector(".weather-info");
 
 const currentWeather = async (place) => {
-    weatherInfo.textContent = "Loading...";
+    // weatherInfo.textContent = "Loading...";
 
-    try {
-        const data = await fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${String(
-                place
-            )}&appid=${API_KEY}&units=metric`
-        );
+    const data = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${String(
+            place
+        )}&appid=${API_KEY}&units=metric`
+    );
 
-        return await data.json();
-    } catch {
-        return 0;
-    }
+    return await data.json();
 };
 
 const placeInput = document.querySelector(".info-form");
 const mainHeader = document.querySelector(".main-header");
 const weatherImage = document.querySelector(".weather-image");
 
+const temperature = document.querySelector(".temperature");
+const feels = document.querySelector(".feels");
+
 placeInput.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     let place = document.querySelector(".place").value;
-    // let weatherData = await currentWeather(
-    //     document.querySelector(".place").value
-    // );
+    let weatherData = await currentWeather(
+        document.querySelector(".place").value
+    );
 
-    // if (weatherData == 0) {
-    //     weatherInfo.textContent = "Error";
-    //     return;
-    // }
+    if (Number(weatherData.cod) == 404) {
+        weatherImage.src = "";
+        temperature.textContent = String(weatherData.message);
+        feels.textContent = "";
+        return;
+    }
 
-    // if (Number(weatherData.cod) == 404) {
-    //     weatherInfo.textContent = String(weatherData.message);
-    //     return;
-    // }
+    weatherImage.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    temperature.textContent = `${weatherData.main.temp}° C`;
+    feels.textContent = weatherData.weather[0].main;
 
     // weatherInfo.textContent = JSON.stringify(weatherData);
-    // weatherImage.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 
-    weatherInfo.textContent = "Loaded.";
+    // weatherInfo.textContent = "Loaded.";
 
     mainHeader.textContent = `${place}'s Weather`;
 });
